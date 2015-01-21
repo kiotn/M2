94d257563d6ec108a5934c3fdb9c7c6acc37a01f
azhuang3 <zhuangamy@yahoo.com>
2015-01-21 17:21:13 -0500

    edit p5

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -77,9 +77,9 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("gburdell4");
                   
                   //Person5 replace P5 with your name
-                  //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+        //and gburdell5 with your gt id
+        Person5 p5 = new Person5("Amy Zhuang");
+        toPrint += p5.toString("azhuang3");
                   
                   //Person6 replace P6 with your name
                   //and gburdell6 with your gt id
